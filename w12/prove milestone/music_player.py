5f4cd079ffbc714f0cb4f74d26fcb8e1fb597df3
simphiwe.nkabinde-buyi <nka22001@byui.edu>
2022-07-20 00:28:50 +0200

    create full music player UI

--- w12/prove milestone/music_player.py
+++ w12/prove milestone/music_player.py
@@ -1,15 +1,12 @@
 import tkinter as tk
-from tkinter.font import BOLD
-
-from pyparsing import col
 
 def main():
     # create the TK root object
     root = tk.Tk()
-    root.geometry("350x550")
+    root.geometry("570x525")
 
     #create the main frame.
-    frm_main = tk.Frame(root, bd=0, bg="gray10", padx=20, pady=20)
+    frm_main = tk.Frame(root, bd=0, bg="gray10")
     frm_main.master.title("Music Player")
     frm_main.pack(fill=tk.BOTH, expand=1)
 
@@ -41,12 +38,15 @@ def populate_main_window(frm_main):
 
     # labels
     lbl_playin_from = tk.Label(frm_main, text="Playing from Playlist", fg="ivory2", bg="gray10")
-    lbl_headphones = tk.Label(frm_main, image=img_headphones, bg="gray15", width=280, height=280)
-    lbl_song = tk.Label(frm_main, text="We are the champions", fg="ivory2", bg="gray10", font=BOLD)
+    lbl_headphones = tk.Label(frm_main, image=img_headphones, bg="gray10", width=280, height=280)
+    lbl_song = tk.Label(frm_main, text="We are the champions", fg="ivory2", bg="gray10", font="bold")
     lbl_artist = tk.Label(frm_main, text="Queen", fg="ivory4", bg="gray10")
 
+    # list box
+    list_songs = tk.Listbox(frm_main, selectmode="single", bg="gray8", fg="ivory2", bd=0, highlightthickness=0, selectbackground="gray12", width=36, height=28)
+
     # buttons
-    btn_back = tk.Button(frm_main, cursor="hand2", text="<", bd=0, fg="ivory3", bg="gray10", highlightthickness=0, activebackground="gray10", activeforeground="ivory4")
+    btn_add_songs = tk.Button(frm_main, cursor="hand2", text="Add Songs", bd=0, fg="ivory3", bg="DarkOrchid4", width=33, highlightthickness=0, activeforeground="ivory4", activebackground="purple4")
     btn_play = tk.Button(frm_main, cursor="hand2", image=img_play_btn, bd=0, bg="gray10", highlightthickness=0, activebackground="gray10")
     btn_pause = tk.Button(frm_main, cursor="hand2", image=img_pause_btn, bd=0, bg="gray10", highlightthickness=0, activebackground="gray10")
     btn_next = tk.Button(frm_main, cursor="hand2", image=img_next_btn, bd=0, bg="gray10", highlightthickness=0, activebackground="gray10")
@@ -54,7 +54,6 @@ def populate_main_window(frm_main):
 
 
     # grid layout
-    btn_back.grid(row=0, column=0)
     lbl_playin_from.grid(row=0, column=1, columnspan=11)
 
     lbl_headphones.grid(row=1, column=0, columnspan=12, pady=20)
@@ -66,6 +65,11 @@ def populate_main_window(frm_main):
     btn_play.grid(row=4, column=3, columnspan=6, pady=20)
     btn_pause.grid(row=4, column=3, columnspan=6, pady=20)
     btn_next.grid(row=4, column=9, columnspan=3, pady=3)
+
+    btn_add_songs.grid(row=0, column=13)
+    list_songs.grid(row=1, rowspan=4, column=13)
+
+
 
 # If this file is executed like this:
 # > python heart_rate.py
