1c20ce6c906d65b4117c7ba867ee553e2fbff93b
simphiwe.nkabinde-buyi <nka22001@byui.edu>
2022-07-20 13:13:18 +0200

    modify test function

--- w12/prove milestone/test_music_player.py
+++ w12/prove milestone/test_music_player.py
@@ -3,8 +3,10 @@ import pytest
 
 def test_trim_song_name():
     # trim length
-    MAX_LEN = 20
-    # list of untrimmed song names
+    MAX_LEN_20 = 20
+    MAX_LEN_10 = 10
+
+    # list of original song names
     song_names = [
         '06 Come Thou Fount (feat. Paul Carda).mp3',
         '09 Homeward Bound.mp3',
@@ -13,15 +15,25 @@ def test_trim_song_name():
         'melody.mp3'
     ]
     # list of corresponding trimmed names. names are trimmed to max_len = 20
-    trimmed_names = [
+    trimmed_names_max20 = [
         '06 Come Thou Fount (...',
         '09 Homeward Bound',
         'The_Temperature_of_t...',
         'Gabriel Rios - Gold ...',
         'melody'
     ]
+    # list of corresponding trimmed names. names are trimmed to max_len = 10
+    trimmed_names_max10 = [
+        '06 Come Th...',
+        '09 Homewar...',
+        'The_Temper...',
+        'Gabriel Ri...',
+        'melody'
+    ]
+
     for i in range(len(song_names)):
-        assert trim_song_name(song_names[i], MAX_LEN) == trimmed_names[i]
+        assert trim_song_name(song_names[i], MAX_LEN_20) == trimmed_names_max20[i]
+        assert trim_song_name(song_names[i], MAX_LEN_10) == trimmed_names_max10[i]
 
 
 # Call the main function that is part of pytest so that the
